864377771229a82c28f60702b2678e997be7fc50
Kretov Ivan <ivan.kretov93@mail.ru>
2020-12-18 11:00:40 +0300

    start

---
+++ src/model/Account.java
@@ -0,0 +1,8 @@
+package model;
+
+import java.math.BigDecimal;
+
+public class Account {
+    BigDecimal balance;
+    AccountStatus accountStatus;
+}
---
+++ src/model/TransactionStatus.java
@@ -0,0 +1,9 @@
+package model;
+
+public enum TransactionStatus {
+    FINISHED,
+    REVIEW,
+    BLOCKED,
+    RETURNED;
+
+}
